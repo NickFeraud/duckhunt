203e5ce1fd7aeccd3e67c755b5e68b546b792508
NickFeraud <nickferaud@gmail.com>
2015-04-06 16:50:33 -0400

    Added motion to the flight, split up backgrounds work into two classes

--- src/Background.java
+++ src/Background.java
@@ -7,12 +7,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
-
 import javax.swing.Timer;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -20,32 +15,56 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Background extends JPanel implements ActionListener{
+public class Background {
+	public static void main (String [] args){
+		JFrame frame = new JFrame( "Duck Hunt");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GamePanel gpanel = new GamePanel();
+	    
+	    frame.add(gpanel);
+	    frame.setSize(new Dimension(1600, 1000));
+		frame.setSize(805, 505);
+		frame.setVisible(true);
+		//doesn't allow user to resize frame
+		frame.setResizable(false);
+	}
+}
 
+class GamePanel extends JPanel implements ActionListener{
+
+	private boolean wingsdown = true;
 	private final int MIN_X = 38;
 	private final int MAX_X = 770;	//750;				//779; the duck went off the edge
 	private final int MIN_Y = 35; 			//this one somehow effects the ground not the top		
 	private final int MAX_Y = 345;			//above the ground
-	private final int INC_AMOUNT = 100;		//we can inc this by 10 each round and cap at 100?
+	private final int INC_AMOUNT = 35;		//we can inc this by 10 each round and cap at 100?
 	private int DELAY = 300;
 	protected Timer timer;
 	private Random random= new Random();
 	private int x = random.nextInt(MAX_X);
 	private int y = random.nextInt(MAX_Y);
-	
 	private int dx = INC_AMOUNT;
 	private int dy = INC_AMOUNT;
-	
 	private BufferedImage bg;			//background
-	private BufferedImage duck;			//duck
+	private BufferedImage inflight,duck1, duck2;	//duck
+	private BufferedImage bar10,bar9,bar8,bar7,bar6,bar5,bar4,bar3,bar2,bar1;		//full bar
+	private BufferedImage ammo3,ammo2,ammo1,ammo0;
 	public void actionPerformed(ActionEvent e)
-	{	repaint();	}
+	{	
+		wingsdown = !wingsdown;
+		inflight = wingsdown ? duck1: duck2;
+		repaint();	
+	}
 	
-	public Background(){
+	public GamePanel(){
 		try{
 			bg = ImageIO.read(getClass().getResource("background.jpg"));	//800x500
-			duck =ImageIO.read(getClass().getResource("flyingduck.png")); 
-		
+			duck1 =ImageIO.read(getClass().getResource("flyingduck.png")); 
+			duck2 =ImageIO.read(getClass().getResource("flyingduck2.png")); 
+			bar10 = ImageIO.read(getClass().getResource("bar10.png"));
+			ammo3 = ImageIO.read(getClass().getResource("Ammo1.png"));
+			
 		}catch (IOException ex ){}
 		
 		timer = new Timer(DELAY,this);	//(delay,this);  timer is what will redraw
@@ -55,11 +74,6 @@ public class Background extends JPanel implements ActionListener{
 	
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		/*
-		if (x < radius)			dx = Math.abs(dx);
-		if (x > getWidth() - radius)	dx = -Math.abs(dx);
-		if (y < radius)			dy = Math.abs(dy);
-		if (y > getHeight() - radius)	dy = -Math.abs(dy);*/
 		
 		//check boundaries
 		if (x <  MIN_X)			dx = Math.abs(dx);
@@ -71,51 +85,9 @@ public class Background extends JPanel implements ActionListener{
 		x += dx;
 		y += dy;
 		g.drawImage(bg,0,0,null);
-		g.drawImage(duck, x-MIN_X, y-MIN_Y, null);
+		g.drawImage(bar10, 305, 426, null);
+		g.drawImage(ammo3, 25, 426, null);
+		g.drawImage(inflight, x-MIN_X, y-MIN_Y, null);
 	}
-	
-	public static void main (String [] args){
-		JFrame frame = new JFrame( "Duck Hunt");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		Background bpanel = new Background();
-	    bpanel.setPreferredSize(new Dimension(800, 500));
-	    bpanel.setBackground(Color.BLACK);
-	    bpanel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-	    Box box = new Box(BoxLayout.Y_AXIS);
-
-	    box.add(Box.createVerticalGlue());
-	    box.add(bpanel);     
-	    box.add(Box.createVerticalGlue()); // causes a deformation
-	    
-	    frame.add(bpanel);
-	    frame.setSize(new Dimension(1600, 1000));
-		frame.setSize(805, 505);
-		frame.setVisible(true);
-		//doesn't allow user to resize frame
-		frame.setResizable(false);
-	}
-	
-	
-    /*JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
-    panel.setPreferredSize(new Dimension(100, 100));
-    panel.setBackground(Color.RED); // for debug 
-
-    panel.setAlignmentX(JComponent.CENTER_ALIGNMENT); // have no effect
-
-    Box box = new Box(BoxLayout.Y_AXIS);
-
-    box.add(Box.createVerticalGlue());
-    box.add(panel);     
-    box.add(Box.createVerticalGlue()); // causes a deformation
-
-    frame.add(box);
-    frame.setSize(new Dimension(200, 200));
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);*/
-
-	
-	
 	
 }
